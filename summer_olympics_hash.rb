9fd6426badb0e802abbc0f435b1f631b2f547677
erikamorais <erikamorais2001@gmail.com>
2018-08-09 20:12:03 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -31,6 +31,7 @@ def add_a_key_value_pair
   summer_olympics [new_place] = new_date
   puts summer_olympicS
 end
+add_a_key_value_pair
 
 def iterate_through_hash
   # Implement this method so that it iterates over the hash created in add_a_key_value_pair
@@ -43,5 +44,9 @@ end
 def iterate_through_keys
   # Implement this method so that it converts the keys of the hash created in add_a_key_value_pair
   # into uppercased Strings, creates a new array with these values, and then puts each upcase key in the CLI
-
+puts olympics_hash
+olympics_hash.each do |sports,year|
+  puts "Sports: #{sports} Year: #{year}"
 end
+end
+iterate_through_keys
