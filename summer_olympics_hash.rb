2bf8363580a0ca2d938fc2af64fd91c187280087
erikamorais <erikamorais2001@gmail.com>
2018-08-09 20:16:45 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -44,7 +44,8 @@ end
 def iterate_through_keys
   # Implement this method so that it converts the keys of the hash created in add_a_key_value_pair
   # into uppercased Strings, creates a new array with these values, and then puts each upcase key in the CLI
-olympics_hash = 0 
+olympics_hash = 0
+counter = 0 
 puts olympics_hash
 olympics_hash.each do |sports,year|
   puts "Sports: #{sports} Year: #{year}"
